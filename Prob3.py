361b79fd5f42303a9fb012ba7171a749a8bd55c4
Sddyke <sddyke@willamette.edu>
2024-10-14 23:06:15 -0700

    Update Prob3.py

--- Prob3.py
+++ Prob3.py
@@ -1,7 +1,7 @@
 ########################################
-# Name:
-# Collaborators:
-# Estimate time spent (hrs):
+# Name: Sam Dyke
+# Collaborators: Emma(Quad Center)
+# Estimate time spent (hrs): 3ish hrs.
 ########################################
 
 from pgl import GWindow, GRect, GLabel, GLine
@@ -13,21 +13,51 @@ SQUARE_SIZE = 50                    # Width and height of square
 SCORE_DX = 10                       # Distance from left of window to origin
 SCORE_DY = 10                       # Distance up from bottom of window to baseline
 SCORE_FONT = "bold 40pt 'serif'"    # Font for score
-
+color='red'
 def clicky_box():
 
-    # Defining the callback function, which you won't need until Part C
-    def on_mouse_down(event):
-        print("You clicked the window!") # Delete this once you start Part C
 
+    
+    def on_mouse_down(e):
+    	x = e.get_x() #gets x pos of mouse
+    	y = e.get_y() #gets y pos of mouse
+    	bx= gw.box.get_x() #gets x pos of box.
+    	by= gw.box.get_y() #gets y pos of box.
 
-    # Down here you should initialize the window and draw the initial square
-    # Make sure you tab it in so that it is part of the clicky_box function
+    	if x in range(int(bx),int(bx+SQUARE_SIZE)): #checks if mouse x pos is within square
+    		if y in range(int(by),int(by+SQUARE_SIZE)): #checks if mouse y pos is within square.
+    			gw.box.set_location(get_r(),get_r()) #moves box to new random location.
+    			gw.score+=1 #adds 1 point to the score.
+    			label() #runs label function
+    		else:
+    			gw.score-=gw.score #resets score when box y is missed.
+    			label()
+    	else:
+    		gw.score-=gw.score #resets score when box x is missed.
+    		label()
+    	
+    gw = GWindow(GW_WIDTH,GW_HEIGHT)
+#    gw.box = GRect(225,225,50,50)
+    gw.box = GRect(GW_WIDTH/2-SQUARE_SIZE/2,GW_HEIGHT/2-SQUARE_SIZE/2,SQUARE_SIZE,SQUARE_SIZE) 
+#*** when above line is used, instead of gw.box = GRect(225,225,50,50), for some reason the 
+#range functions from lines 27 and 28 require the numbers to become integers, but does not when I
+#use commented out line 40, in other words, the int function isn't required if I use that.
+    gw.box.set_filled(True)
+    gw.box.set_color(color)
+    gw.add(gw.box)
+    gw.score=0 #sets score as 0 initially
+    gw.label=GLabel(f"{gw.score}",SCORE_DX,GW_HEIGHT-SCORE_DY)
+    gw.label.set_font(SCORE_FONT)
+    gw.add(gw.label)
+    def label(): #this function removes the previous label and replaces it with one displaying the new score.
+    	gw.remove(gw.label)
+    	gw.label=GLabel(f"{gw.score}",SCORE_DX,GW_HEIGHT-SCORE_DY)
+    	gw.label.set_font(SCORE_FONT)
+    	gw.add(gw.label)
+    gw.add_event_listener("click",on_mouse_down)
 
-    gw = GWindow(GW_WIDTH, GW_HEIGHT)
-
-
-
+def get_r():
+    return random.randint(0, GW_HEIGHT-SQUARE_SIZE)
 
 
 
